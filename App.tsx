dac8f6cf1e470c3baf8f734411ae4b55af560408
Raj Mhatre <raj.m@sankeysolutions.com>
2026-01-12 16:16:50 +0530

    Added Taliwind css setup

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import "./global.css"
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -10,6 +11,10 @@ const HomeScreen = ({ navigation }: any) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 20, marginBottom: 16 }}>Home Screen</Text>
+
+       <Text className="text-2xl font-bold text-red-500">
+        Welcome to Nativewind!
+      </Text>
 
       <Button
         title="Go to Test File"
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,12 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const config = mergeConfig(defaultConfig, {
+  // you can add custom metro config here later
+});
+
+module.exports = withNativeWind(config, {
+  input: './global.css',
+});
